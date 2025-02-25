f52e9f815e265f79b6e22c95c49975eee0adb6ed
Milan Snoeijink <snoeijinkmilan@gmail.com>
2025-02-25 16:14:56 +0100

    Temp Captcha Disabled (fix)

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
     const [formData, setFormData] = useState<{ name: string; email: string; message: string }>({ name: "", email: "", message: "" });
     const [isSending, setIsSending] = useState(false);
     const [success, setSuccess] = useState<boolean | null>(null);
-    const [captchaValue, setCaptchaValue] = useState<string | null>(null);
+    // const [captchaValue, setCaptchaValue] = useState<string | null>(null);
 
     useEffect(() => {
         if (success !== null) {
@@ -114,12 +114,12 @@ const Contact = () => {
                                     className="w-full px-3 py-2 text-black bg-gray-50 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
                                 ></textarea>
                             </div>
-                            <div className="mb-4 flex justify-center">
+                            {/* <div className="mb-4 flex justify-center">
                                 <ReCAPTCHA
                                     sitekey="6LcuW-IqAAAAAHTXkiZXRdhwTeXyXvxeGPIsZ9ZH"
                                     onChange={(value) => setCaptchaValue(value)}
                                 />
-                            </div>
+                            </div> */}
                             <button
                                 type="submit"
                                 disabled={isSending}
